95437ca2e3db2f5663473579f3daf052a6bb3349
James Ireland <James.Ireland@softwire.com>
2025-06-17 17:27:20 +0100

    part 2 up to adding 13

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,17 @@
-var rules = [{'factor': 3,
-              'text': 'Fizz'},
-             {'factor': 5,
-              'text': 'Buzz'}];
+var rules = [{'test': (x) => x%3 === 0,
+              'result': (s) => s+'Fizz'}, //append 'Fizz'
+             {'test': (x) => x%5 === 0,
+              'result': (s) => s+'Buzz'}, //append 'Buzz'
+             {'test': (x) => x%7 === 0,
+              'result': (s) => s+'Bang'}, //append 'Bang'
+             {'test': (x) => x%11 === 0,
+              'result': (s) => 'Bong'}, //replace the text so far with 'Bong'
+             {'test': (x) => x%13 === 0,
+              'result': (s) => {var i = s.indexOf('B'); //insert 'Fezz' before the first occurrence of 'B'
+                                if (i < 0) {i = s.length;}
+                                return s.slice(0,i)+'Fezz'+s.slice(i,s.length)}}];
+
+                                //replace output string with array of strings for easy reversal of order
 
 // This is our main function
 function fizzbuzz() {
@@ -13,14 +23,14 @@ function fizzbuzz() {
     for (let i = start; i <= end; i++) {
         var outputText = '';
         for (let rule of rules) {
-            if (i%rule.factor == 0) {
-                outputText = outputText + rule.text;
+            if (rule.test(i)) {
+                outputText = rule.result(outputText);
             }
         }
-        if (outputText.length == 0) {
+        if (outputText.length === 0) {
             outputText = i.toString();
         }
-        console.log(outputText)
+        console.log(i.toString() + ': ' + outputText)
     }
 
     // Put your code here...
