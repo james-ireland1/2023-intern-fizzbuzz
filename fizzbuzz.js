5c076cf48620385efd43540684437dd57488e47c
James Ireland <James.Ireland@softwire.com>
2025-06-17 16:19:51 +0100

    first attempt part 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,27 @@
+var rules = [{'factor': 3,
+              'text': 'Fizz'},
+             {'factor': 5,
+              'text': 'Buzz'}];
+
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
+    console.log("Starting...");
+    
+    const start = 1;
+    const end = 100;
+
+    for (let i = start; i <= end; i++) {
+        var outputText = '';
+        for (let rule of rules) {
+            if (i%rule.factor == 0) {
+                outputText = outputText + rule.text;
+            }
+        }
+        if (outputText.length == 0) {
+            outputText = i.toString();
+        }
+        console.log(outputText)
+    }
 
     // Put your code here...
 
